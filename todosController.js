9af4eabdaec79aebcd0c5161dc4809f3ca383ae3
TTBoiMike <michaelthorpe747@hotmail.com>
2021-01-13 19:16:43 +0000

    push app

---
+++ todosController.js
@@ -0,0 +1,54 @@
+const createError = require('http-errors')
+
+let todoList = []
+let idno = 0
+
+exports.index = function (req, res) {
+    res.send({key: "item", arr: todoList})
+}
+
+exports.create = function (req, res, next) {
+    console.log("request", req)
+    console.log("res", res)
+    if(!req.body.name) {
+        return (next(createError(400, "name is required")))
+    }
+    // console.log(req, res)
+    todoList.push({id: idno, name: req.body.name})
+    res.send({result: true})
+    idno++
+}
+
+exports.show = function (req, res, next) {
+    const todoItem = todoList.find((todo) => todo.id == req.params.id)
+    if(!todoItem) {
+        return (next(createError(404, "no todo available with that id")))
+    }
+    res.send(todoItem)
+}
+
+exports.delete = function (req, res, next) {
+    const todoItem = todoList.find(todo => todo.id !== req.params.id)
+    if(!todoItem) {
+        return (next(createError(404, "no todo available with that id")))
+    }
+    todoList = todoList.filter(todo => todo.id != req.params.id)
+    res.send({result: true})
+}
+
+exports.update = function (req, res, next) {
+    const todoItem = todoList.find((todo) => todo.id == req.params.id)
+    if(!req.body.name) {
+        return (next(createError(400, "name is required")))
+    }
+    if(!todoItem) {
+        return (next(createError(400, "no todo with that id")))
+    }
+    todoList = todoList.map((todo) => {
+        if(todo.id == req.params.id) {
+            todo.name = req.body.name
+        }
+        return todo
+    })
+    res.send({result: true})
+}
